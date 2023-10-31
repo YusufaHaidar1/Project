ee585893ff001c6dd03a3feed997277266671563
Yusufa Haidar <91399445+PencuriHandal@users.noreply.github.com>
2023-10-31 10:47:03 +0700

    Update Challenge

--- uts/lib/screens/Home.dart
+++ uts/lib/screens/Home.dart
@@ -200,7 +200,7 @@ class _HomePageState extends State<Home> {
                     SizedBox(
                       height: 100,
                       child: ListView.separated(
-                        padding: EdgeInsets.only(left: 20),
+                        padding: EdgeInsets.only(left: 15),
                         itemCount: negara.length, 
                         scrollDirection: Axis.horizontal,
                         separatorBuilder: (_, __) => const SizedBox(width: 20),
